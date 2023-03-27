be771a4fa3fee884d76e5542fcf1fae3cd6ae639
ImJustChew <chewtzihwee@gmail.com>
2023-03-28 01:55:51 +0800

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<script>
+    import "virtual:windi.css"
+</script>  
+
+<div class="w-screen h-screen grid grid-cols-[14rem,auto] grid-rows-[70px,auto] overflow-hidden bg-light-400">
+    <div class="row-span-2 bg-white p-6 border-r-gray-300 border-r">
+        <img src="/nthu.png" class="w-16 h-16" alt="nthu logo"/>
+        <div class="py-4">
+            <h1 class="text-2xl font-semibold">國立清華大學</h1>
+            <h1 class="text-lg font-semibold">NTHU</h1>
+        </div>
+        <div class="">
+            <input type="text" class="w-full bg-gray-200 rounded-md py-2 px-4 text-sm font-semibold" placeholder="Quick Search (Ctrl + /)" />
+            
+        </div>
+    </div>
+    <div class="flex flex-row bg-white px-6 items-center">
+        <h2 class="flex-1 text-xl font-bold">主頁 Dashboard</h2>
+        <div class="grid grid-cols-[48px,auto] gap-3 place-items-center">
+            <div class="grid place-items-center w-12 h-12">
+                <img src="/nthu.png" class="" alt="user icon"/>
+            </div>
+            <div class="flex flex-col gap-1">
+                <span class="font-semibold leading-none">丘子輝</span>
+                <span class="text-sm font-semibold leading-none">Chew Tzi Hwee</span>
+            </div>
+        </div>
+    </div>
+    <div class="overflow-auto">
+        <slot />
+    </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<div class="grid grid-cols-3 gap-4 p-4">
+    <div class="col-span-2 bg-white rounded-xl py-4 px-6">
+        <h2 class="text-2xl font-bold">課程 Lectures</h2>
+        <h4 class="text-sm font-semibold text-gray-500">ONGOING NOW</h4>
+        <div class="p-4 bg-[#F6EAFF] rounded-lg">
+            <h2 class="text-2xl font-bold text-[#8F51BE]">離散數學 Discrete Mathematics</h2>
+            <p class="text-black">10:10am - 12:00pm</p>
+            <p>PROF. WING KAI HON</p>
+            <p>Venue: DELTA 107</p>
+        </div>
+    </div>
+    <div class="col-span-1 bg-white rounded-xl py-4 px-6">
+        <h2 class="text-2xl font-bold">行事曆 Schedule</h2>
+        <div class="flex flex-col divide-x divide-gray-300">
+            <div class="flex flex-row pt-2 gap-2 items-center">
+                <div class="flex flex-col items-center">
+                    <div class="font-semibold text-lg leading-none">12</div>
+                    <p class="text-sm ">3/2022</p>
+                </div>
+                <div class="flex flex-col">
+                    <div class="font-semibold text-sm leading-none">作業 Homework</div>
+                    <p class="font-semibold">Homework 4: Bubble Sort</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
